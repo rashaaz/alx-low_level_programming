cb432a8b7c3f5433ee4ef8613b5dcd170388a604
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-10 15:52:57 -0700

    add  101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program prints string
+ *
+ * Return: Always 1 (!Success)
+*/
+
+int main(void)
+{
+	char tsck []="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,tsck,59);
+
+return (1);
+}
