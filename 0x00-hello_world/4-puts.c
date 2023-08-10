48e5d3a591cf614223cfae60be11dc05eb75ae28
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-10 11:08:26 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 	 *Description: A C program that prints with put Function
 	 *
 	 * Return: Always 0 (Success)
-	 */
+	*/
 	int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
