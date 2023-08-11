3b5284c74edf681e111f3302a6fa91cdbcd954ad
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-10 17:38:48 -0700

    add 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: program prints string
+ * Description: program that prints size
  *
- * Return: Always 1 (!Success)
+ * Return: Always0 (Success)
 */
 
 int main(void)
 {
-	char tsck []="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,tsck,59);
+	char tsck[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, tsck, 59);
 	return (1);
 }
