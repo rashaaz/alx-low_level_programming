31906d0a602e14faf9670420c7c46cd661d79cbe
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-11 06:15:44 -0700

    add 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program whit if
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar('l');
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
