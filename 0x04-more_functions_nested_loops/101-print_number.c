2e720d8e80317c1b9ff3abc5d76a0cf0de90e223
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-17 10:25:43 -0700

    add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer whit Recursion
+ * @n: The number to be printed.
+*/
+
+void print_number(int n)
+{
+	unsigned int kk = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		kk = -kk;
+	}
+	else if ((kk / 10) > 0)
+	{
+		print_number(kk / 10);
+	}
+	else
+	{
+		_putchar((kk % 10) + 48);
+	}
+}
