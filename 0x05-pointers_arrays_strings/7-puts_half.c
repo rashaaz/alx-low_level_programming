5d985013a1872866e81adf12915c821ce7826ec0
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-21 04:12:01 -0700

    add 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - Print the second half
+ * @str: Pointer to the string
+*/
+void puts_half(char *str)
+{
+	int ii, jj;
+
+	for (ii = 0; str[ii] != '\0'; ii++)
+	{
+
+	}
+	for (jj = ii / 2; str[jj] != '\0'; jj++)
+	{
+		_putchar(str[jj]);
+	}
+	_putchar('\n');
+}
