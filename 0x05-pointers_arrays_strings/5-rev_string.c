33a19b7ed51f455249d46e447f0e4370feba0156
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-21 03:05:22 -0700

    add 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: Pointer to the string
+*/
+
+void rev_string(char *s)
+{
+	int ii, le;
+	char tp;
+	for (le = 0; s[le] != '\0'; le++)
+	{
+
+	}
+	for (ii = 0; ii < le / 2; ii++)
+	{
+		tp = s[ii];
+		s[ii] = s[le - ii - 1];
+		s[le - ii - 1] = tp;
+	}
+}
