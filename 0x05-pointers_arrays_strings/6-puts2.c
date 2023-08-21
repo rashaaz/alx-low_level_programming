aa5c8882b469f46e12d1ee897ae07c489bd0d7ec
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-21 03:47:04 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int ii;
 
-	for (ii = 0; str[ii] != 0; ii += 2)
-	{
-		_putchar(str[ii]);
-	}
+	for (ii = 0; str[ii] != '\0'; ii++)
+		if (ii % 2 == 0)
+		{
+			_putchar(str[ii]);
+		}
 	_putchar('\n');
 }
