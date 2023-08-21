28acac3a597e7411beb0e8b2d2b826eb7d5261db
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-21 02:14:51 -0700

    add 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int ii, jj;
+	int ii;
 
 	for (ii = 0; s[ii] != 0; ii++)
 	{
 
 	}
-	for (jj = ii - 1; ii >= 0; ii--)
+	for (ii = ii - 1; ii >= 0; ii--)
 	{
 		_putchar(s[ii]);
 	}
