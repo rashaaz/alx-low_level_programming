f312b1d0d87563f9543a9ee939e714abeaf6c208
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-23 10:03:02 -0700

    add 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,34 +1,29 @@
 #include "main.h"
 
 /**
- * leet - encodes a string
- * @ra: a string
+ * leet - encodes
+ * @ra: input
  *
- * Return: a pointer
- */
-
+ * Return: pointer
+*/
 char *leet(char *ra)
 {
-	int ii;
+	char *sh = "aAeEoOtTlL";
+	char *en = "4433007711";
 
-	char *leet(char *ra)
+	int ii, jj;
+
+	for (ii = 0; ra[ii] != '\0'; ii++)
 	{
-		char *sh = "aAeEoOtTlL";
-		char *en = "4433007711";
-
-		int ii, jj;
-
-		for (ii = 0; ra[ii] != '\0'; ii++)
+		for (jj = 0; sh[jj] != '\0'; jj++)
 		{
-			for (jj = 0; sh[jj] != '\0'; jj++)
+			if (ra[ii] == sh[jj])
 			{
-				if (ra[ii] == sh[jj])
-				{
-					ra[ii] = en[jj];
-					break;
-				}
+				ra[ii] = en[jj];
+				break;
 			}
 		}
-
-		return (ra);
 	}
+
+	return (ra);
+}
