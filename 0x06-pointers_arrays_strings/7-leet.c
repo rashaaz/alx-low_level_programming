a9156e26d59dfbc6b4b4b7fb1b3262bf81ee330e
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-23 09:52:51 -0700

    add 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,29 +11,24 @@ char *leet(char *ra)
 {
 	int ii;
 
-	for (ii = 0; ra[ii] != '\0'; ii++)
+	char *leet(char *ra)
 	{
-		if (ra[ii] == 'a' || ra[ii] == 'A')
-		{
-			ra[ii] = '4';
-		}
-		if (ra[ii] == 'e' || ra[ii] == 'E')
-		{
-			ra[ii] = '3';
-		}
-		if (ra[ii] == 'o' || ra[ii] == 'O')
-		{
-			ra[ii] = '0';
-		}
-		if (ra[ii] == 't' || ra[ii] == 'T')
-		{
-			ra[ii] = '7';
-		}
-		if (ra[ii] == 'l' || ra[ii] == 'L')
-		{
-			ra[ii] = '1';
-		}
-	}
+		char *sh = "aAeEoOtTlL";
+		char *en = "4433007711";
 
-	return (ra);
-}
+		int ii, jj;
+
+		for (ii = 0; ra[ii] != '\0'; ii++)
+		{
+			for (jj = 0; sh[jj] != '\0'; jj++)
+			{
+				if (ra[ii] == sh[jj])
+				{
+					ra[ii] = en[jj];
+					break;
+				}
+			}
+		}
+
+		return (ra);
+	}
