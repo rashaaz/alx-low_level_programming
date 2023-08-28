428e1d91c35c3c6b8aec2bd7b03123cd02e7ba45
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-28 04:00:42 -0700

    add 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,9 +12,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int ii, jj;
 
-	for (ii = 0; s[ii] >= '\0'; ii++)
+	for (ii = 0; s[ii] != '\0'; ii++)
 	{
-		for (jj = 0; accept[jj] >= '\0'; jj++)
+		for (jj = 0; accept[jj] != '\0'; jj++)
 		{
 			if (s[ii] == accept[jj])
 			{
@@ -22,6 +22,4 @@ char *_strpbrk(char *s, char *accept)
 			}
 		}
 	}
-
-	return (NULL);
 }
