b1054c00b9eca3a0e4e4c6f08ac2236623b228fe
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-28 06:14:43 -0700

    add 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints 2d arrry
+ * @a: pointer
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int ii, jj;
+
+	for (ii = 0; ii < 8; ii++)
+	{
+		for (jj = 0; jj < 8; jj++)
+		{
+			_putchar(a[ii][jj]);
+		}
+		_putchar('\n');
+	}
+}
