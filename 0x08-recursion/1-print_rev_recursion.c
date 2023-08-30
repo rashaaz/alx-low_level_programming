c70416ef0a87d748ae5bfac139a68e2da1b89a07
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-30 01:22:15 -0700

    add  1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - Prints a reverse
+ * @s: pointer
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
+}
