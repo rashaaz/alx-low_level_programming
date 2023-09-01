22729119f9a07651e28da43d048100c3afb37770
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-01 02:49:56 -0700

    add new corect

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,6 @@
  *
  * Return: Always 0 (Success) else 1
 */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -19,23 +18,16 @@ int main(int argc, char *argv[])
 	}
 	else if (argc == 2)
 	{
-		int ii, r, m;
-
-		ii;
-		r = 0;
-		m = atoi(argv[1]);
+		int r = 0;
+		int m = atoi(argv[1]);
 		int sh[5] = {25, 10, 5, 2, 1};
 
-		for (ii = 0; ii < 5; ii++)
+		for (int ii = 0; ii < 5; ii++)
 		{
 			if (m >= sh[ii])
 			{
 				r += m / sh[ii];
 				m = m % sh[ii];
-				if (m % sh[ii] == 0)
-				{
-					break;
-				}
 			}
 		}
 		printf("%d\n", r);
