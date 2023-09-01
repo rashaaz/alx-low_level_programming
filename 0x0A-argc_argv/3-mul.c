c5b47fa5a15b48ba655a83065729c16f9ca7524b
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-31 23:10:16 -0700

    add 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print mul of argv[1] and [2]
+ * @argc: number of the thing you inter
+ * @argv: array of number of the thing you inter
+ *
+ * Return: 0 (Success), else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int m;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else if (argc == 3)
+	{
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
+
