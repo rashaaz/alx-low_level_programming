2d5a9e476181ebb3433fbc9a585b0f399aa01879
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-01 02:28:02 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,11 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	int m;
 
 	if (argc == 3)
 	{
-		m = atoi(argv[1]) * atoi(argv[2]);
+		int m = atoi(argv[1]) * atoi(argv[2]);
+
 		printf("%d\n", m);
 	}
 	else
