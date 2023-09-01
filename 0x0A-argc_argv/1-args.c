e520de0d74659028c3f0942eab6dac3188d5aa02
rashaaz <rashaabdelaziz111@gmail.com>
2023-08-31 22:44:04 -0700

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print value of argc - 1
+ * @argc: number of the thing you inter
+ * @argv: array of number of the thing you inter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
