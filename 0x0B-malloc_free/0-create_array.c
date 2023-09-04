d8d2c1e99e0003f84a5c73cec5f466932b97fcf2
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-03 23:36:15 -0700

    add README.md, main.h, _putchar.c and  0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - make an array of characters
+ * @size: the size
+ * @c: charcter
+ *
+ * Return: 0 if size == 0 or r == NULL else r
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (0);
+	}
+	int i;
+	char *r;
+
+	r = malloc(sizeof(char) * size);
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		r[i] = c;
+	}
+	return (r);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+alx tasks 
+0x0B. C - malloc, free
