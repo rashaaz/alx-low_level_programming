41dd01223c3c5e4ca934c38c3cbdecf4c9ff4ad2
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-10 22:34:11 -0700

    add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this prints name of the file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
