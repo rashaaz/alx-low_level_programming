9152728116388ddac8ce9e4c0e6d65c5bcd38b8a
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-11 00:04:51 -0700

    add README.md and dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+alx tasks 
+0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new dogs
+ * @name: The dog name
+ * @age: The dog age
+ * @owner: the dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
