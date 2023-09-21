5642120f4fa33cec5aaa90aeaf7ed513f74cb157
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-21 02:32:24 -0700

    add 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
 section		.text
-extern	printf
-global 	main
+	extern	printf
+	global 	main
 main:
 move		edi, prt
 move		eax, 0
 call	printf
 
 section		.data
-prt db	'Hello, Holberton' , 0xa, 0
+	prt db	'Hello, Holberton', 0xa, 0
