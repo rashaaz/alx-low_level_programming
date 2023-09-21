fef8d44b928d8de5403343319a27aebdbd137fc4
rashaaz <rashaabdelaziz111@gmail.com>
2023-09-21 00:11:07 -0700

    add  0-print_list.c ,  lists.h, putchar.c , README.md

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints the elements
+ * @h: Pointer
+ *
+ * Return: number
+ */
+size_t print_list(const list_t *h)
+{
+	size_t co = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		co++;
+		h = h->next;
+	}
+
+	return (co);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+alx tasks
+0x12. C - Singly linked lists
