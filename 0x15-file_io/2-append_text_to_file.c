29f0b73be5f3e1a6458436a07c01280c5a0ed834
rashaaz <rashaabdelaziz111@gmail.com>
2023-10-01 21:37:36 -0700

    add 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _len - Calculate length
+ * @c: Pointer
+ *
+ * Return: Length
+ */
+
+int _len(char *c)
+{
+	int x = 0;
+
+	if (!c)
+		return (0);
+
+	while (*c++)
+		x++;
+	return (x);
+}
+
+/**
+ * append_text_to_file - append file
+ * @filename: file name
+ * @text_content: The content
+ *
+ * Return: 1 , or -1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int num;
+	ssize_t b = 0, n = _len(text_content);
+
+	if (!filename)
+		return (-1);
+	num = open(filename, O_WRONLY | O_APPEND);
+	if (num == -1)
+		return (-1);
+	if (n)
+		b = write(num, text_content, n);
+	close(num);
+	return (b == n ? 1 : -1);
+
+}
