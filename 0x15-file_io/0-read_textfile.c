d53e6bde3a01c5435ae00d26cb052ed332f2ef65
rashaaz <rashaabdelaziz111@gmail.com>
2023-10-01 21:19:57 -0700

    add 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * read_textfile - read file
+ * @filename: file name
+ * @letters: The size
+ *
+ * Return: b
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int num;
+	ssize_t b;
+	char buf[my_buf * 8];
+
+	if (!filename || !letters)
+		return (0);
+	num = open(filename, O_RDONLY);
+	if (num == -1)
+		return (-1);
+	b = read(num, &buf[0], letters);
+	b = write(STDOUT_FILENO, &buf[0], b);
+	close(num);
+	return (b);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+alx tasks
+0x15. C - File I/O
