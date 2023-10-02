323de2067c802259d4d3828e76a0ba5ae45bc004
rashaaz <rashaabdelaziz111@gmail.com>
2023-10-02 06:03:16 -0700

    modfy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-#define USAGE "Usage: cp file_from_to\n"
+#define USAGE "Usage: cp file_from file_to\n"
 #define EOR_NOM "Error: Can't read from file %s\n"
 #define EOR_NUMW "Error: Can't write to %s\n"
 #define EOR_NUMcol "Error: Can't close fd %d\n"
-#define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
 /**
  * main - Entry point
@@ -21,25 +21,37 @@ int main(int argc, char **argv)
 	char buf[my_buf];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, USAGE), exit(97);
+	}
 	num = open(argv[1], O_RDONLY);
 	if (num == -1)
+	{
 		dprintf(STDERR_FILENO, EOR_NOM, argv[1]), exit(98);
+	}
 	num1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERM);
 	if (num1 == -1)
+	{
 		dprintf(STDERR_FILENO, EOR_NUMW, argv[2]), exit(99);
-
+	}
 	while ((b = read(num, buf, my_buf)) > 0)
+	{
 		if (write(num1, buf, b) != b)
+		{
 			dprintf(STDERR_FILENO, EOR_NUMW, argv[2]), exit(99);
+		}
+	}
 	if (b == -1)
+	{
 		dprintf(STDERR_FILENO, EOR_NOM, argv[1]), exit(98);
+	}
 	num = close(num);
 	num1 = close(num1);
 	if (num)
+	{
 		dprintf(STDERR_FILENO, EOR_NUMcol, num), exit(100);
+	}
 	if (num1)
-		dprintf(STDERR_FILENO, EOR_NUMcol, num), exit(100);
-
+		dprintf(STDERR_FILENO, EOR_NUMcol, num1), exit(100);
 	return (EXIT_SUCCESS);
 }
