1a18cd3e4ec87e0916f6118ad16d31c3e89f53c7
rashaaz <rashaabdelaziz111@gmail.com>
2023-11-08 23:06:50 -0800

    add 103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,41 +11,41 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	char password[7], *co;
+	int len = strlen(argv[1]), ii, tp;
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	co = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
+	tp = (len ^ 59) & 63;
+	password[0] = co[tp];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
+	tp = 0;
+	for (ii = 0; ii < len; ii++)
+		tp += argv[1][ii];
+	password[1] = co[(tp ^ 79) & 63];
 
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
+	tp = 1;
+	for (ii = 0; ii < len; ii++)
+		tp *= argv[1][ii];
+	password[2] = co[(tp ^ 85) & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
+	tp = 0;
+	for (ii = 0; ii < len; ii++)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][ii] > tp)
+			tp = argv[1][ii];
 	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
+	srand(tp ^ 14);
+	password[3] = co[rand() & 63];
 
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
+	tp = 0;
+	for (ii = 0; ii < len; ii++)
+		tp += (argv[1][ii] * argv[1][ii]);
+	password[4] = co[(tp ^ 239) & 63];
 
-	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
+	for (ii = 0; ii < argv[1][0]; ii++)
+		tp = rand();
+	password[5] = co[(tp ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
